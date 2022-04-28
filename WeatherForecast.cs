4cfbd1082c382eb5eafaec2c8a477120ee0f953f
Lakasabasz <lakasabasz@gmail.com>
2022-04-28 16:29:52 +0200

    Init

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Książka_Adresowa;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
